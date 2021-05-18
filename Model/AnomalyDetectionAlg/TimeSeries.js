0293224ba8d234d7c38bc0808d9231acb317145b
Matrucci <matan.saloniko@gmail.com>
2021-05-18 17:31:32 +0300

    Added anomaly detection

---
+++ Model/AnomalyDetectionAlg/TimeSeries.js
@@ -0,0 +1,28 @@
+class TimeSeries {
+    constructor(CsvJson) {
+        this.csvMap = new Map();
+        this.featureTitles = [];
+        this.csvMap = JSON.parse(JSON.stringify(CsvJson));
+        this.featureTitles = Object.keys(this.csvMap);
+        this.columns = this.csvMap[this.featureTitles[0]].length;
+        this.rows = this.featureTitles.length;
+    }
+
+    getColumn(key) {
+        return typeof this.csvMap[key] !== "undefined" ? this.csvMap[key] : [];
+    }
+
+    getColumnSize() {
+        return this.columns;
+    }
+
+    getRowSize() {
+        return this.rows;
+    }
+
+    getFeatures() {
+        return this.featureTitles;
+    }
+}
+
+module.exports = {TimeSeries};
--- Model/anomalyHandler.js
+++ Model/anomalyHandler.js
@@ -1,3 +1,5 @@
+const {TimeSeries} = require("./AnomalyDetectionAlg/TimeSeries");
+
 function  findAnomalies(train, test, key){
     let result='1\n'
     train.split("\n").forEach(row=>{
@@ -21,6 +23,72 @@ function  findAnomalies(train, test, key){
 function sendToServer(input) {
     //TODO send to server and build JSON
     return "A-B     135"
+}
+
+
+
+
+class AnomalyManager{
+    constructor(detector) {
+        this._detector = detector;
+        this._train = null;
+        this._test = null;
+    }
+
+    uploadTrain(trainJson){
+        this._train = new TimeSeries(trainJson);
+    }
+
+    uploadTest(testJson){
+        this._test = new TimeSeries(testJson);
+    }
+
+    learn(){
+        this._detector.learnNormal(this._train);
+    }
+
+    detect(){
+        this._detector.detect(this._test);
+    }
+
+    mostCorrelative(column){
+        let cf = this._detector._cf;
+        for(let corr of cf) {
+            if (corr._feature1 === column) {
+                return corr._feature2;
+            }
+        }
+        return "no most correlative";
+    }
+
+    getAnomalies(column){
+        let cf = this._detector._cf;
+        for (let c of cf){
+            if (c._feature1 === column){
+                let prevTimeStep = 0;
+                let isFirst = true;
+                let anomalies = [];
+                let str = [];
+                for (let timeStep of c._anomaliesTimeStep){
+                    if (isFirst || timeStep-prevTimeStep > 1){
+                        if (!isFirst){
+                            str.push(prevTimeStep);
+                            anomalies.push(str);
+                            str = []
+                        }
+                        str.push(timeStep);
+                        isFirst = false;
+                    }
+                    prevTimeStep = timeStep;
+                }
+                if (!isFirst){
+                    str.push(prevTimeStep);
+                    anomalies.push(str);
+                }
+                return anomalies;
+            }
+        }
+    }
 }
 
 module.exports.findAnomalies = findAnomalies
